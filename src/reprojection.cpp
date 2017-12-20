860c0fcf866de95cc4e072c714e09949d1e60520
tx1 <ugasmallsatlab@gmail.com>
2017-12-19 19:19:31 -0500

    stableish
    Former-commit-id: 55288d557074d71796406acaf48b7d5472f3d5ed

--- src/reprojection.cpp
+++ src/reprojection.cpp
@@ -1,11 +1,11 @@
-//======================================//
-// UGA SSRL Reprojection                //
-// Author: Caleb Adams                  //
-// Contact: CalebAshmoreAdams@gmail.com //
-//======================================//
+//#======================================#//
+//# UGA SSRL Reprojection                #//
+//# Author: Caleb Adams                  #//
+//# Contact: CalebAshmoreAdams@gmail.com #//
+//#======================================#//
 // A seriously good source:
 // https://developer.nvidia.com/sites/default/files/akamai/cuda/files/Misc/mygpu.pdf
-// 
+//
 // This program is only meant to perform a 
 // small portion of MOCI's science pipeline
 //
@@ -18,8 +18,17 @@
 #include <sstream>
 #include <string>
 #include <vector>
+// my boiz @ nvidia
 #include <cuda_runtime.h>
+//#include "cublas.h"
 #include "cublas_v2.h"
+//#include "cusolver.h"
+// to remove eventually
+#include <time.h>
+
+// alsp remove eventually
+#define N 8192
+#define  BILLION  1000000000L;
 
 using namespace std;
 
@@ -40,16 +49,16 @@ unsigned short camera_count;
 
 // This was for the test cases only 
 unsigned int   res  = 1024;
-float          dpix = 0.00002831538; //(foc*tan(fov/2))/(res/2)
 float          foc  = 0.035;
-float          fov  = 0.857550075; // 49.134 degrees  // 0.785398163397; // 45 degrees
+float          fov  = 0.8575553107; // 49.1343 degrees  // 0.785398163397; // 45 degrees
 float          PI   = 3.1415926535;
+float          dpix = (foc*tan(fov/2))/(res/2); //float          dpix = 0.00002831538; //(foc*tan(fov/2))/(res/2)
 
 // Test 2 with slightly higher res
 //unsigned int   res  = 2000;
 //float          dpix = 0.00002831538; //(foc*tan(fov/2))/(res/2)
 //float          foc  = 0.035;
-//float          fov  = 0.857550075; // 49.134 degrees  // 0.785398163397; // 45 degrees
+//float          fov  = 0.8575553107; // 49.1343 degrees  // 0.785398163397; // 45 degrees
 //float          PI   = 3.1415926535;
 
 // this is for the blender sim of mnt everest
@@ -300,7 +309,7 @@ float get_angle(float cam[6], int flag){
   float angle = acos(fract);
   // check to see if outside of second quad
   if (cam[4] < 0.0) angle = 2 * PI - angle;
-  if (angle > max_angle) max_angle = angle;
+  if (angle >max_angle) max_angle = angle;
   if (angle < min_angle) min_angle = angle;
   return angle;
 }
@@ -433,11 +442,11 @@ void two_view_reproject_plane(){
     float camera2[6] = {stof(cameras[image2-1][1]),stof(cameras[image2-1][2]),stof(cameras[image2-1][3]),stof(cameras[image2-1][4]),stof(cameras[image2-1][5]),stof(cameras[image2-1][6])};
     // scale the projection's coordinates
     float x1 = dpix * (stof(matches[i][2]) - res/2.0);
-    float y1 = dpix * (stof(matches[i][3]) - res/2.0);
+    float y1 = dpix * (-1*stof(matches[i][3]) + res/2.0);
     float x2 = dpix * (stof(matches[i][4]) - res/2.0);
-    float y2 = dpix * (stof(matches[i][5]) - res/2.0);
+    float y2 = dpix * (-1*stof(matches[i][5]) + res/2.0);
 
-    // NOTE FROM HERE ON THERE ARE THINGS for the rotation
+    // NOTE: FROM HERE ON THERE ARE THINGS for the rotation
 
     // rotate the coords to be correct
     if (debug && 0){ // just set to not do this for now
@@ -455,8 +464,8 @@ void two_view_reproject_plane(){
     vector<float> k1 = rotate_projection_x(x1,y1,0.0,PI/2);
     vector<float> k2 = rotate_projection_x(x2,y2,0.0,PI/2);
 
-    vector<float> kp1 = rotate_projection_z(k1[0],k1[1],k1[2],r1 + PI/2);
-    vector<float> kp2 = rotate_projection_z(k2[0],k2[1],k2[2],r2 + PI/2);
+    vector<float> kp1 = rotate_projection_z(k1[0],k1[1],k1[2],r1 + PI/2.0); // + PI/2.0
+    vector<float> kp2 = rotate_projection_z(k2[0],k2[1],k2[2],r2 + PI/2.0); // + PI/2.0
 
     // adjust the kp's location
     kp1[0] = camera1[0] - (kp1[0] + (camera1[3] * foc));
@@ -468,7 +477,6 @@ void two_view_reproject_plane(){
     float points1[6] = {camera1[0],camera1[1],camera1[2],kp1[0],kp1[1],kp1[2]};
     float points2[6] = {camera2[0],camera2[1],camera2[2],kp2[0],kp2[1],kp2[2]};
     int   rgb[3]     = {stoi(matches[i][6]),stoi(matches[i][7]),stoi(matches[i][8])};
-
 
     // this is just for storing the projections for a ply file later
     vector<float> r32;
@@ -489,7 +497,7 @@ void two_view_reproject_plane(){
     float j_holder = 0.0;
     float p1[3]; //= {0.0,0.0,0.0};
     float p2[3]; //= {0.0,0.0,0.0};
-    for (float j = 0.0; j < 800.0; j += 0.00001){
+    for (float j = 0.5; j < 300.0; j += 0.00001){
       p1[0] = points1[0] + v1[0]*j;
       p1[1] = points1[1] + v1[1]*j;
       p1[2] = points1[2] + v1[2]*j;
@@ -497,27 +505,17 @@ void two_view_reproject_plane(){
       p2[1] = points2[1] + v2[1]*j;
       p2[2] = points2[2] + v2[2]*j;
       float dist = euclid_dist(p1,p2);
-      if (j > 100.0) cout << "LARGE NUMBER: " << j << endl;
       if (dist < smallest) smallest = dist;
       else {
-      	j_holder = j;
-      	goto breaker;
+	j_holder = j;
+	goto breaker;
       }
-       //for (float j = 50.0; j < 800.0; j += 0.0001){
-	 // get the points on the lines
-       // p2[0]  = points2[0] + v2[0]*j;
-       // p2[1]  = points2[1] + v2[1]*j;
-       // p2[2]  = points2[2] + v2[2]*j;
-       //float dist = euclid_dist(p1,p2);
-       //if (dist > smallest) smallest = dist;
-       //else goto breaker;
-       //}
     }
   breaker:
     cout << "smallest: " << smallest << ", j: [" << j_holder << "]" << endl;
     // store the result if it sasifies the boundary conditions
     // TODO uncomment this after you test to see how far those points go
-    //if (p1[2] > 1.4 && p1[2] < 2.7){
+    if (j_holder > 1.0){
       vector<float> v;
       vector<int>   c;
       v.push_back(p1[0]);
@@ -529,7 +527,7 @@ void two_view_reproject_plane(){
       if (debug) cout << p1[0] << "," << p1[1] << "," << p1[2] << endl;
       points.push_back(v);
       colors.push_back(c);
-    // }
+    }
     if (verbose) cout << (((((float)i))/((float)length)) * 100.0) << " \%" << endl;
   }
   cout << "Generated: " << points.size() << " valid points" << endl;
@@ -599,33 +597,6 @@ int main(int argc, char* argv[])
   if (to_pan) two_view_reproject_pan();
   else two_view_reproject_plane();
   save_ply();
-
-  //vector scale test
-  /*
-  float *x = new float[19000*4];
-  x=( float *) malloc (19000* sizeof (*x)); // host memory alloc for x
-
-  int j; // index of elements
-  for(j=0;j<19000;j++)
-  {
-      x[j]=( float )j; // x={0 ,1 ,2 ,3 ,4 ,5}
-  }
-  printf ("x:\n");
-  for(j=0;j<19000;j++)
-  {
-      printf (" %2.0f,",x[j]); // print x
-  }
-  printf ("\n");
-
-  cout << "the test value is: " << x[3*4+6] << endl;
-
-
-  // ary[i][j] is then rewritten as
-  // ary[i*sizeY+j]
-  vector_scale(x, 19000, 4, 124.0f);
-
-  cout << "the test value is now: " << x[3*4+6] << endl;
-  */
 
   if (verbose) cout << "done!\nresults saved to output.ply" << endl;
   if (debug) cout << "max angle: " << max_angle << " | min angle: " << min_angle << endl;
