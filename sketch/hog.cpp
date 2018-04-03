ade1901faa9ce88ad8be698d745b6cacef471db8
Jacob C. Conley <jake@jakeconley.com>
2018-04-03 15:53:53 -0400

    Complete first draft of HOG; yet untested
    Former-commit-id: 2227ee6198c537e79c48233a53fc7408c2a68743

--- sketch/hog.cpp
+++ sketch/hog.cpp
@@ -5,103 +5,170 @@
 #include <iostream>
 #include <math.h>
 
-//TEMP
-double norm(double x, double y){ return sqrt(x * x + y * y); } // Defined in CUDA
-
  // Utilities
+
+void assert(bool assertion){ if(!assertion) std::cout << "ASSERTION FAILED" << std::endl; exit(1); }
 
 typedef struct { double x; double y; } Vector;
 typedef struct { double ang; double mag; } Polar;
 
-#define FOREACH_X for(int x = 0; x < imgX; x++)
-#define FOREACH_Y for(int y = 0; y < imgY; y++)
-#define FOREACH_X_IN(a,b) for(int x = a; x < b; x++)
-#define FOREACH_Y_IN(a,b) for(int y = a; y < b; y++)
-
-const double TO_DEG 			= 57.29577;	// 180/pi
+const double TO_DEG 			= 57.29577;		// 180/pi
 const double PI 				=  3.14159;
+const double SPC				= 0.001; 		// "Small positive constant"
 
 
 // Constants & parameters
 // Defined as in Tomasi's paper
-const int HIST_WINDOW_SIZE		= 8; // "C"
-const int HIST_BINS				= 9; // "B"
-
-// Globals.  Replace this however you want to
-//TODO: Allocation here
-int 			imgX, imgY; 		// Size of image			--		Let Window = { (x,y) | x ∈[0,imgX) ∧ y ∈[0,imgY) }
-int ** 			I; 					// Image function			--		(x,y) ∈ Window		|-->	[0,255]
-Polar ** 		G;					// Gradient 				--		(x,y) ∈ Window		|-->	θ ∈[0,2π] , r
+const int CELL_SIZE			= 8; // "C"
+const int HIST_BINS			= 9; // "B"
 
 
 //
 // -- -- --
 //
 
-
-//
-// Approximate the gradient.  Tomasi does this by central differences, for reasons enumerated in his paper
-//
-void ComputeGradient()
+int main(int argc, char ** argv)
 {
+	//TODO: Input
+
+	//TODO: Allocation here
+	// Global allocations
+	int XX, YY;							// Input size in X and Y in pixels
+	int ** 			Image; 				// Image function			--		(x,y) ∈ Window		|-->	[0,255]
+	Polar ** 		Gradient;			// Gradient 				--		(x,y) ∈ Window		|-->	θ ∈[0,2π] , r
+	double ** 		CellHistograms;		// [0, numCells) |--> [0, HIST_BINS) |--> double
+	double ** 		BlockHistograms;	// [0, numBlock) |--> [0, HIST_BINS) |--> double
+
+	double * 		FeatureOutput;
+
+
+	// --
+	// Peripheral computations
+	// --
+
+	// Gotta be divisible by the cell size !!!!
+	assert(XX % CELL_SIZE == 0);
+	assert(YY % CELL_SIZE == 0); 
+
+	int XC 			= XX / CELL_SIZE;  int YC = YY / CELL_SIZE;		// Number of cells 	in X and Y
+	int XB 			= (XC - 1); int YB = (YC - 1);					// Number of blocks in X and Y
+	int numCells 	= XC * YC;										// Total number of cells
+	int numBlocks 	= XB * YB;										// Total number of blocks
+
+	int FeatureOutputSize = numBlocks * 4 * HIST_BINS; // For each bin in each block.  There are 4 cells per block.
+
+
+	// --
+	// HOG Computations
+	// --
+
+	// -----------
+	// Step 1: Compute gradients
+	// Approximate the gradient.  Tomasi does this by central differences, for reasons enumerated in his paper
 	//TODO: Address boundary conditions here.  Out of range for x ∈{0,imgX} , same with y
-	for(int x = 0; x < imgX; x++) { for(int y = 0; y < imgY; y++) {
+	for(int x = 0; x < XX; x++) { for(int y = 0; y < YY; y++) {
 
-		static float dx = I[x+1][y] - I[x-1][y];
-		static float dy = I[x][y+1] - I[x][y-1];
+		static float dx = Image[x+1][y] - Image[x-1][y];
+		static float dy = Image[x][y+1] - Image[x][y-1];
 
-		//CUDA: norm and atan2.  mod as well?
-		Polar * g = &(G[x][y]);
-		g->mag = norm(dx, dy);						
+		//CUDA: g->mag computation, and atan2
+		Polar * g = &(Gradient[x][y]);
+		g->mag = sqrt(dx*dx + dy*dy);
 		g->ang = fmod(atan2(dx,dy), PI) * TO_DEG; 
 		// MOCI GANG MOCI GANG MOCI GANG MOCI GANG MOCI GANG MOCI GANG MOCI GANG
 
 	} }
-}
 
-/** Compute the cell orientation histogram over a square window @(wx, wy) with size HIST_WINDOW_SIZE.
- * This is the first histogram generation step.
- *
- * @param result Histogram array: int [0,HIST_BINS) |--> double 
- * @param wx Window x coordinate
- * @param wy Window y coordinate
- */
-void ComputeCellOrientationHistogram(double * result, double wx, double wy)
-{
-	static const float width = 180 / HIST_BINS; // "w"
+	// -----------
+	// Step 2: Compute cell orientation histograms
+	// Split the image up into "cells", CELL_SIZE x CELL_SIZE windows, of which there are (XC * YC = numCells).  
+	for(int i = 0; i < numCells; i++) 
+	{
+		static const float width = 180 / HIST_BINS; // "w"
 
-	for(int x = wx; x < wx + HIST_WINDOW_SIZE; x++) { for(int y = wy; y < wy + HIST_WINDOW_SIZE; y++ ) {
-
-		Polar * g = &(G[x][y]);
-
-		int bin1 		= fmod(floor((g->ang / width) - 0.5 ),  HIST_BINS);			// "j"			-- Bin index
-		double ctr1 	= width * (bin1 + 1 + 0.5);									// "c_(j+1)"	-- Center, of bin j+1
-		double vot1 	= g->mag * (( ctr1 - g->ang ) / width);						// "v"			-- Bin vote
-
-		int bin2		= fmod(bin1 + 1,  HIST_BINS);								// "j+1"		-- Bin index
-		double ctr2 	= width * (bin1 + 0.5);										// "c_j"		-- Center, of bin j
-		int vot2		 = g->mag * (( g->ang - ctr2 ) / width);					// "v_(j+1)"	-- Bin vote
-
-		result[bin1] = vot1; 
-		result[bin2] = vot2;
-
-	} }
-}
+		// For each cell
+		int cx = i % XC;									// x index of cell
+		int cy = (i - cx) / XC;								// y index of cell
+		int wx = cx * CELL_SIZE, wy = cy * CELL_SIZE;		// x and y coordinates of cell windows
 
 
-int main(int argc, char ** argv)
-{
-	//TODO: Input 
+		for(int x = wx; x < wx + CELL_SIZE; x++) { for(int y = wy; y < wy + CELL_SIZE; y++ ) {
 
-	//TODO: Allocation here
-	int 		numKeypoints;	// 
-	int * 		KeypointsX;		// [0, numKeypoints) |--> int
-	int *		KeypointsY;		// [0, numKeypoints) |--> int
-	double **	Histograms;		// [0, numKeypoints) |--> [0, HIST_BINS) |--> double;
+			Polar * g = &(Gradient[x][y]);
+
+			int bin1 		= (int) floor((g->ang / width) - 0.5 ) %  (int) HIST_BINS;	// "j"			-- Bin index
+			double ctr1 	= width * (bin1 + 1 + 0.5);									// "c_(j+1)"	-- Center, of bin j+1
+			double vot1 	= g->mag * (( ctr1 - g->ang ) / width);						// "v"			-- Bin vote
+
+			int bin2		= (bin1 + 1) %  HIST_BINS;									// "j+1"		-- Bin index
+			double ctr2 	= width * (bin1 + 0.5);										// "c_j"		-- Center, of bin j
+			double vot2		= g->mag * (( g->ang - ctr2 ) / width);						// "v_(j+1)"	-- Bin vote
+
+			CellHistograms[i][bin1] = vot1; 
+			CellHistograms[i][bin2] = vot2;
+
+		} }
+	}
+
+	// -------------
+	// Step 3: Block normalization
+	// Combine each group of four neighboring cells into overlapping blocks, of which there are (XB * YB = numBlock).
+	// For each block, concatenate the histograms of the cells within into a single histogram.  Normalize this
+	#define FOREACH_BIN(j) for(int j = 0; j < HIST_BINS; j++)
+	for(int i = 0; i < numBlocks; i++)
+	{
+		// For each block; h1-4 are the histograms of the cells the blocks include
+		double * block = BlockHistograms[i];
+		int b = floor(i / XB);
+		double * h1 = CellHistograms[b+i],  * h2 = CellHistograms[b+i+1], * h3 = CellHistograms[b+i+XC], * h4 = CellHistograms[b+i+XC+1];
+
+		// Concatenate & normalize
+		//CUDA: There is a norm function in CuBLAS, but it does not include the small positive constant below.  What do?
+		double norm = 0.0;
+		FOREACH_BIN(j)
+		{
+			double d = h1[j] + h2[j] + h3[j] + h4[j];
+			block[j] = d;
+			norm += (d * d);
+		}
+		norm = sqrt(norm + SPC); // "Small positive constant" to prevent division by zero, as in Tomasi's paper
+		FOREACH_BIN(j) block[j] /= norm;
+	}
+	#undef FOREACH_BIN
+
+	// -------------
+	// Step 4: Vector normalization
+	// This happens according to a pretty bizarre scheme, which is, of course, defined in Tomasi's paper.
+	// His arguments, as they are with all of these things, are entirely empirical.
+
+	#define FOREACH_BIN { int i = -1; for(int blk = 0; blk < numBlocks; blk++){ for(int bin = 0; bin < HIST_BINS; bin++){ i++; 
+
+	{
+		// Construct and normalize it the first time
+		double norm = 0.0;
+		FOREACH_BIN {
+			double d = BlockHistograms[blk][bin];
+			FeatureOutput[i] = d; 
+			norm += (d*d);
+		} } } }
+		FOREACH_BIN { FeatureOutput[i] /= norm; } } } } 
+	}
+
+	// Clip everything to a threshold of 0.2
+	// This ensures that "very large gradients do not have too much influence"
+	FOREACH_BIN { double d = FeatureOutput[i]; FeatureOutput[i] = (0.2 < d ? 0.2 : d); } } } }
+
+	{
+		// Normalize it again
+		double norm = 0.0;
+		FOREACH_BIN { norm += (FeatureOutput[i]); } } } }
+		FOREACH_BIN { FeatureOutput[i] /= norm; } } } } 
+	}
 
 
-	ComputeGradient();	
-	for(int i = 0; i < numKeypoints; i++)  ComputeCellOrientationHistogram( Histograms[i],   KeypointsX[i], KeypointsY[i]   );
+	// -----
+	// DONE!
+	// -----
 
 	//TODO: Output
 
