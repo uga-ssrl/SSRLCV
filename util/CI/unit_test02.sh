3a9c5c0205a907064f3cce513f6cea5c29d2d8d5
deff not carl wheezer from that one show <piepieninja@gmail.com>
2018-05-21 18:19:22 +0000

    updates cube test
    Former-commit-id: 537f3494c5c3db65c3bc613d436739de8c43eb56

--- util/CI/unit_test02.sh
+++ util/CI/unit_test02.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "running cube test..."
+make -j4
 ./bin/2viewreprojection.x data/2view/cube_cameras.txt data/2view/cube_matches.txt
 mv output.ply output_gpu.ply
 ./bin/2viewreprojection.x data/2view/cube_cameras.txt data/2view/cube_matches.txt 0
 mv output.ply output_cpu.ply
 diff output_cpu.ply output_gpu.ply
+make clean
