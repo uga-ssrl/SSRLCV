e00fbd9d18547898e4f3d4280ac16447cc040053
deff not carl wheezer from that one show <piepieninja@gmail.com>
2018-05-21 18:40:16 +0000

    update unit_test02.sh
    Former-commit-id: 33f3c728bf922aa5bbed38c6ece58c4a6a3ec7ef

--- util/CI/unit_test02.sh
+++ util/CI/unit_test02.sh
@@ -7,7 +7,8 @@ echo "running cube test..."
 make -j4
 ./bin/2viewreprojection.x data/2view/cube_cameras.txt data/2view/cube_matches.txt
 
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
     echo OK
 else
     echo FAIL
@@ -17,7 +18,8 @@ fi
 #mv output.ply output_gpu.ply
 ./bin/2viewreprojection.x data/2view/cube_cameras.txt data/2view/cube_matches.txt 0
 
-if[ $? -eq 0 ]; then
+if[ $? -eq 0 ]
+then
     echo OK
 else
     echo FAIL
