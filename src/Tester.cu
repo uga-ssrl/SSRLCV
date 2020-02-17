44995a2b2018a3c1da604873b8b000387a7592d0
Andrew Decker <ajd94001@uga.edu>
2020-02-17 14:22:33 -0500

    Tested some functionality of n-view
    Former-commit-id: 8984458685bdb199df1a98d8a0f7f3995e51b247

--- src/Tester.cu
+++ src/Tester.cu
@@ -5,6 +5,50 @@
 #include "MatchFactory.cuh"
 #include "PointCloudFactory.cuh"
 #include "MeshFactory.cuh"
+#include "matrix_util.cuh"
+
+float3 nView(int numLines, ssrlcv::Bundle::Line* lines){
+  //Initializing Variables
+  float3 S [3];
+  float3 C;
+  S[0] = {0,0,0};
+  S[1] = {0,0,0};
+  S[2] = {0,0,0};
+  C = {0,0,0};
+  //Iterating through the Lines in a Bundle
+  for(int i = 0; i < numLines; i++){
+    ssrlcv::Bundle::Line L1 = lines[i];
+    float3 tmp [3];
+    ssrlcv::normalize(L1.vec);
+    ssrlcv::matrixProduct(L1.vec, tmp);
+    //Subtracting the 3x3 Identity Matrix from tmp
+    tmp[0].x -= 1;
+    tmp[1].y -= 1;
+    tmp[2].z -= 1;
+    //Adding tmp to S
+    S[0] = S[0] + tmp[0];
+    S[1] = S[1] + tmp[1];
+    S[2] = S[2] + tmp[2];
+    //Adding tmp * pnt to C
+    float3 vectmp;
+    ssrlcv::multiply(tmp, L1.pnt, vectmp);
+    C = C + vectmp;
+  }
+  /**
+   * If all of the directional vectors are skew and not parallel, then I think S is nonsingular.
+   * However, I will look into this some more. This may have to use a pseudo-inverse matrix if that
+   * is not the case.
+   */
+  float3 Inverse [3];  
+  if(ssrlcv::inverse(S, Inverse)){
+    float3 point;
+    ssrlcv::multiply(Inverse, C, point);
+    return point;
+  }else{
+    std::cout << "Error" << std::endl;
+    exit(1);
+  }
+}
 
 int main(int argc, char *argv[]){
   try{
@@ -13,155 +57,24 @@ int main(int argc, char *argv[]){
     clock_t totalTimer = clock();
     clock_t partialTimer = clock();
 
-    std::cout << "=========================== TEST 01 ===========================" << std::endl;
-    std::cout << "Creating Fake Images" << std::endl;
-    std::vector<ssrlcv::Image*> images_vec;
+    //Declare Lines
+    ssrlcv::Bundle::Line * lp = new ssrlcv::Bundle::Line[3];
+    ssrlcv::Bundle::Line l1;
+    ssrlcv::Bundle::Line l2;
+    ssrlcv::Bundle::Line l3;
+    l1.pnt = {1, 0, 0};
+    l1.vec = {0, 1, 0};
+    l2.pnt = {-1, 0, 0};
+    l2.vec = {0, 1, 0};
+    l3.pnt = {0, 0, 0};
+    l3.vec = {0, 0, 1};
+    lp[0] = l1;
+    lp[1] = l2;
+    lp[2] = l3;
 
-    // If you want to test N many matched keypoints, you need at least N many fake cameras
-    ssrlcv::Image* image0 = new ssrlcv::Image();
-    ssrlcv::Image* image1 = new ssrlcv::Image();
-    ssrlcv::Image* image2 = new ssrlcv::Image();
-    ssrlcv::Image* image3 = new ssrlcv::Image();
-    ssrlcv::Image* image4 = new ssrlcv::Image();
-    images_vec.push_back(image0);
-    images_vec.push_back(image1);
-    images_vec.push_back(image2);
-    images_vec.push_back(image3);
-    images_vec.push_back(image4);
-
-    // Test Camera Parameters
-    // If you want to test N many matched keypoints, you need at least N many fake cameras
-    // be sure that you index them correctly in the images_vec
-    // be sure that the id matches the index
-    std::cout << "Filling in Test Camera Params ..." << std::endl;
-    images_vec[0]->id = 0;
-    images_vec[0]->camera.size = {2,2};
-    images_vec[0]->camera.cam_pos = {1.0,0.0,0.0};
-    images_vec[0]->camera.cam_rot = {M_PI/2.0,0.0,0.0};
-    images_vec[0]->camera.fov = {10.0 * (M_PI/180.0), 10.0 * (M_PI/180.0)}; // 10 degrees x and y fov
-    images_vec[0]->camera.foc = 0.25;
-
-    images_vec[1]->id = 1;
-    images_vec[1]->camera.size = {2,2};
-    images_vec[1]->camera.cam_pos = {-1.0,0.0,0.0};
-    images_vec[1]->camera.cam_rot = {0.0, M_PI/2.0, 0.0};
-    images_vec[1]->camera.fov = {10.0 * (M_PI/180.0), 10.0 * (M_PI/180.0)};
-    images_vec[1]->camera.foc = 0.25;
-
-    images_vec[2]->id = 2;
-    images_vec[2]->camera.size = {2,2};
-    images_vec[2]->camera.cam_pos = {0.0,0.0,1.0};
-    images_vec[2]->camera.cam_rot = {0.0, 0.0, M_PI/4.0};
-    images_vec[2]->camera.fov = {10.0 * (M_PI/180.0), 10.0 * (M_PI/180.0)};
-    images_vec[2]->camera.foc = 0.25;
-
-    images_vec[3]->id = 3;
-    images_vec[3]->camera.size = {2,2};
-    images_vec[3]->camera.cam_pos = {1.0,0.0,1.0};
-    images_vec[3]->camera.cam_rot = {M_PI/3.0, M_PI/3.0, 0.0};
-    images_vec[3]->camera.fov = {10.0 * (M_PI/180.0), 10.0 * (M_PI/180.0)};
-    images_vec[3]->camera.foc = 0.25;
-
-    images_vec[4]->id = 4;
-    images_vec[4]->camera.size = {2,2};
-    images_vec[4]->camera.cam_pos = {0.0,1.0,1.0};
-    images_vec[4]->camera.cam_rot = {0.0, M_PI/3.0, M_PI/3.0};
-    images_vec[4]->camera.fov = {10.0 * (M_PI/180.0), 10.0 * (M_PI/180.0)};
-    images_vec[4]->camera.foc = 0.25;
-
-    // Test Match Points
-    std::cout << "Filling in Matches ..." << std::endl;
-    ssrlcv::MatchSet matchSet;
-
-    // lets say we want the following:
-    //    4 sets of matches, so we need to have a count of that
-    int matchesnum = 4;
-    // then, in terms of our matches we want:
-    //    * first match connects 2 keypoints
-    //    * second match connects 3 keypoints
-    //    * third match connets 2 keypoints
-    //    * forth match connets 5 keypoints
-    // we need to calcualte how many keypoitns that is, so:
-    int keypointnum = 2 + 3 + 2 + 5;
-
-    // next we need to allocate memory for these guys
-
-    // matches contain groups of keypoints, which are just the R2 coordinates tha correspond
-    // here is where we use matches num
-    matchSet.matches = new ssrlcv::Unity<ssrlcv::MultiMatch>(nullptr,matchesnum,ssrlcv::cpu);
-    // this is the list of R2 coodrindates
-    // here we want to use the keypoint num
-    matchSet.keyPoints = new ssrlcv::Unity<ssrlcv::KeyPoint>(nullptr,keypointnum,ssrlcv::cpu);
-
-    // now we need to fill in what our matches are
-
-
-
-    // note that   *->host[#] is the memory location of the match information, that is sequential in RAM and counts up
-    matchSet.matches->host[0] = {2,0}; // here we say the number of matches and the starting index of those matches in the keypoints
-                                       // that ends up looking like = {number keypoints in the match, where those keyPoints start}
-                                       // for us we have 2 matches and the matches start at index 0
-    // note that the *->host[#] is the memory location, that's sequential in RAM, so those always count up
-    matchSet.keyPoints->host[0] = {{0}, {1.0, 1.0}}; // { {image number}, {x-y match location} }
-    matchSet.keyPoints->host[1] = {{1}, {1.0, 1.0}};
-
-
-
-    // note that   *->host[#] is the memory location of the match information, that is sequential in RAM and counts up
-    matchSet.matches->host[1] = {3,2}; // here we say the number of matches and the starting index of those matches in the keypoints
-                                       // that ends up looking like = {number keypoints in the match, where those keyPoints start}
-                                       // for us we have 3 matches and the matches start at index 3
-    // note that the *->host[#] is the memory location, that's sequential in RAM, so those always count up
-    matchSet.keyPoints->host[2] = {{0}, {1.0, 1.0}};
-    matchSet.keyPoints->host[3] = {{1}, {1.0, 1.0}};
-    matchSet.keyPoints->host[4] = {{2}, {1.0, 1.0}};
-
-
-
-    // note that   *->host[#] is the memory location of the match information, that is sequential in RAM and counts up
-    matchSet.matches->host[2] = {2,5}; // here we say the number of matches and the starting index of those matches in the keypoints
-                                       // that ends up looking like = {number keypoints in the match, where those keyPoints start}
-                                       // for us we have 2 matches and the matches start at index 5
-    // note that the *->host[#] is the memory location, that's sequential in RAM, so those always count up
-                                       // let's say we want to match the last 2 images this time, let's use image indexes 3 and 4
-    matchSet.keyPoints->host[5] = {{3}, {1.0, 1.0}}; // { {image number}, {x-y match location} }
-    matchSet.keyPoints->host[6] = {{4}, {1.0, 1.0}};
-
-
-
-    // note that   *->host[#] is the memory location of the match information, that is sequential in RAM and counts up
-    matchSet.matches->host[3] = {5,7}; // here we say the number of matches and the starting index of those matches in the keypoints
-                                       // that ends up looking like = {number keypoints in the match, where those keyPoints start}
-                                       // for us we have 5 matches and the matches start at index 7
-    // note that the *->host[#] is the memory location, that's sequential in RAM, so those always count up
-    matchSet.keyPoints->host[7]  = {{0}, {1.0, 1.0}}; // { {image number}, {x-y match location} }
-    matchSet.keyPoints->host[8]  = {{1}, {1.0, 1.0}};
-    matchSet.keyPoints->host[9]  = {{2}, {1.0, 1.0}};
-    matchSet.keyPoints->host[10] = {{3}, {1.0, 1.0}};
-    matchSet.keyPoints->host[11] = {{4}, {1.0, 1.0}};
-
-    // now we can try to make lines
-
-    // Line Generation Test
-    ssrlcv::PointCloudFactory demPoints = ssrlcv::PointCloudFactory();
-    
-    std::cout << "Bundles: " << std::endl;
-    ssrlcv::BundleSet bundleSet = demPoints.generateBundles(&matchSet,images_vec);
-    
-    // prints out generated lines in point vector format
-    std::cout << "<lines start>" << std::endl;
-    for(int i = 0; i < bundleSet.bundles->numElements; i ++){
-      for (int j = bundleSet.bundles->host[i].index; j < bundleSet.bundles->host[i].index + bundleSet.bundles->host[i].numLines; j++){
-        std::cout << "(" << bundleSet.lines->host[j].pnt.x << "," << bundleSet.lines->host[j].pnt.y << "," << bundleSet.lines->host[j].pnt.z << ")    ";
-        std::cout << "<" << bundleSet.lines->host[j].vec.x << "," << bundleSet.lines->host[j].vec.y << "," << bundleSet.lines->host[j].vec.z << ">" << std::endl;
-      }
-      std::cout << std::endl;
-    }
-    
-    //N-View Point Cloud
-    ssrlcv::Unity<float3> *pointcloud;
-    pointcloud = demPoints.nViewTriangulate(bundleSet);
-    ssrlcv::writePLY("out/test.ply",pointcloud);
+    //Test
+    float3 point = nView(3, lp);
+    std::cout << point.x << " " << point.y << " " << point.z << std::endl;
   }
   catch (const std::exception &e){
     std::cerr << "Caught exception: " << e.what() << '\n';
