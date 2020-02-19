2d41b53d3a5f1e65cf476c204b13d6f9ccae79c1
Austin Computer <ugasmallsatlab@gmail.com>
2020-02-19 13:09:19 -0500

    actually added yolo
    Former-commit-id: 6b3bf0c80ff38ac4e641b2a0058461a3c55c65e5

---
+++ src/scripts/segmentation/Pytorch-UNet/incremental_train.sh
@@ -0,0 +1,5 @@
+conda activate torch
+python3 train.py -s 1 -e 2 -l 1e-5 -v 20.0
+python3 train.py -s 1 -e 2 -l 1e-6 -v 20.0 -f checkpoints/CP_epoch3.pth
+python3 train.py -s 1 -e 2 -l 1e-7 -v 20.0 -f checkpoints/CP_epoch3.pth
+python3 train.py -s 1 -e 2 -l 1e-8 -v 20.0 -f checkpoints/CP_epoch3.pth
