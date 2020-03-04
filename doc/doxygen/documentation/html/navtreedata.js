adf0b64c18513037f3f8bddcd1d90563e827bcd8
Jackson Parker <jop80923@uga.edu>
2020-03-04 14:26:24 -0500

    doxy update
    Former-commit-id: d1b0fefb13a7fcafcdaea68c3ba656a75e7ecf6c

--- doc/doxygen/documentation/html/navtreedata.js
+++ doc/doxygen/documentation/html/navtreedata.js
@@ -1,11 +1,56 @@
 var NAVTREE =
 [
-  [ "SSRLCV", "index.html", ]
+  [ "SSRLCV", "index.html", [
+    [ "SSRLCV Contributor Guide", "d1/db6/md_CONTRIB.html", null ],
+    [ "Todo List", "dd/da0/todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.html"
+"annotated.html",
+"d0/d90/group__matching__kernels.html#gad2e2d1891a6dbb4d2483623a9d9082b7",
+"d2/daa/group__feature__detection.html#a602d205257b983f1e32ab4779e784c88",
+"d5/de2/group__pointcloud__kernels.html#gaa21c9ee46d73f4952aa8e51fcd881dd2",
+"d7/d7c/group__mismatch.html#gae1346fedefaf956c68a44e481481038b",
+"d7/dfd/group__comparison.html#ga23dcec64a8140cfbab98a18e3f333fab",
+"d7/dfd/group__comparison.html#ga8e419e83728678173f7b496f66555399",
+"d7/dfd/group__comparison.html#gae7e87bdd3271f74dad1f90cd2a505ef6",
+"d9/d70/group__features.html#d3/d90/structssrlcv_1_1Feature",
+"dc/d47/group__cuda__vector__removes.html#ga42133d389d10b5b1ff5962f6a0533cc2",
+"df/dac/group__meshing.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
