a7756e1274fbf30d74d801f4de267c40db6b5fac
ENM185 <ericnathanmiller@icloud.com>
2022-05-02 16:08:05 -0400

    Fix output

--- scripts/run-script.sh
+++ scripts/run-script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 export j=$(sbatch $1 | awk '{print $4}')
+
 echo "PENDING ${j}"
 while [ $(sacct -j $j -X | tail -n1 | awk '{print $6}') == '----------' ]; do :; done
 while [ $(sacct -j $j -X | tail -n1 | awk '{print $6}') == 'PENDING' ]; do :; done
+
 echo "RUNNING"
-while IFS= read -r line1 || IFS= read -r line2 <&3 || [ $(sacct -j $j -X | tail -n1 | awk '{print $6}') == 'RUNNING' ]; do if [ ${#line1} != 0 ]; then printf '\033[0;34mSTDOUT:\033[0m %s\n' '${line1}'; elif [ ${#line2} != 0 ]; then printf '\033[0;31mSTDERR:\033[0m %s\n' '${line2}'; fi; done < log/build.$j.out 3< log/build.$j.err
+while IFS= read -r line1 || IFS= read -r line2 <&3 || [ $(sacct -j $j -X | tail -n1 | awk '{print $6}') == 'RUNNING' ]; do
+    if [ ${#line1} != 0 ]; then
+        printf '\033[0;34mSTDOUT:\033[0m %s\n' "${line1}";
+    elif [ ${#line2} != 0 ]; then
+        printf '\033[0;31mSTDERR:\033[0m %s\n' "${line2}";
+    fi;
+done < log/build.$j.out 3< log/build.$j.err
+
 echo "DONE"
