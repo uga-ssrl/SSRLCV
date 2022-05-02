9453daaea833a9c234b5661ac6702137d716bc29
ENM185 <ericnathanmiller@icloud.com>
2022-05-02 16:18:06 -0400

    Integrate exit code

--- scripts/run-script.sh
+++ scripts/run-script.sh
@@ -15,4 +15,8 @@ while IFS= read -r line1 || IFS= read -r line2 <&3 || [ $(sacct -j $j -X | tail 
     fi;
 done < log/build.$j.out 3< log/build.$j.err
 
+if [ $(sacct -j $j -X | tail -n1 | awk '{print $6}') != 'COMPLETED' ]; then
+    echo "$(sacct -j $j -X | tail -n1 | awk '{print $6}')";
+    exit 1;
+
 echo "DONE"
